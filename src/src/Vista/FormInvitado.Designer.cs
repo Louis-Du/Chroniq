71930364442377f0bf29ad634067da12d2177732
lukasa133 <ldiazc3@soy.sena.edu.co>
2026-04-25 20:23:00 -0500

    update: cambios en los formularios

--- src/src/Vista/FormInvitado.Designer.cs
+++ src/src/Vista/FormInvitado.Designer.cs
@@ -28,10 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // FormInvitado
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "FormInvitado";
             this.Text = "FormInvitado";
+            this.Load += new System.EventHandler(this.FormInvitado_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- src/src/Vista/FormLider.Designer.cs
+++ src/src/Vista/FormLider.Designer.cs
@@ -28,10 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // FormLider
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "FormLider";
             this.Text = "FormLider";
+            this.Load += new System.EventHandler(this.FormLider_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
