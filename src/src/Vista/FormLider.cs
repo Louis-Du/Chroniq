e3e82de50721e0f78e1dc7e76906f8bf2708aead
lukasa133 <ldiazc3@soy.sena.edu.co>
2026-04-25 20:12:56 -0500

    refactor: FormInvitado y FormLider

---
+++ src/src/Vista/FormLider.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace src.Vista
+{
+    public partial class FormLider : BaseMaterialForm
+    {
+        public FormLider()
+        {
+            InitializeComponent();
+        }
+    }
+}
